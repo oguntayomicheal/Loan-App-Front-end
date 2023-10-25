89820c105827dc77bbc4c8adddcac82ad676589b
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-25 22:04:11 +0100

    Add welcome page

---
+++ src/WelcomePage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+const WelcomePage = () => {
+    return <>
+        <h1>
+            Loan App
+        </h1>
+
+        {/* <Link to="/staffLogin">
+            Staff Login
+        </Link>
+
+        <Link to="/login">
+            Customer Login
+        </Link>
+
+        <Link to="/register">
+            Customer Register
+        </Link> */}
+    
+    
+    
+    </>
+}
+
+
+export default WelcomePage;
