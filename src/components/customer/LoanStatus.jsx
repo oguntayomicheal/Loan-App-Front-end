24a45442a89dd6c254adc019acf4d0eb9004f47f
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-31 00:10:08 +0100

    Update loan status component for customers

--- src/components/customer/LoanStatus.jsx
+++ src/components/customer/LoanStatus.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
-const LoanStatus = () => (
-  <>
-    <NavLink to="/LoanApplicationForm" className="each_nav">
-      Application form
-    </NavLink>
-    <br />
+const LoanStatus = () => {
+  const [loanApplicationsList, setLoanApplicationList] = useState([]);
+  const customerData = localStorage.getItem('customerData');
+  const customerId = (JSON.parse(customerData)).id;
 
-    LoanStatus
-  </>
-);
+  const loanUrl = `http://localhost:3100/api/v1/customers/${customerId}/loan_applications`;
+  console.log(loanUrl);
+
+  const fetchloanApplicationsList = async () => {
+    fetch(loanUrl).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response not OK');
+      }
+      return response.json();
+    }).then((data) => {
+      setLoanApplicationList(data);
+    });
+  };
+
+  useEffect(() => {
+    fetchloanApplicationsList();
+  }, []);
+
+  return (
+    <>
+      <NavLink to="/LoanApplicationForm" className="each_nav">
+        Application form
+      </NavLink>
+      <br />
+      <div>
+        <h2>Loan Status</h2>
+        <ul id="allLoans">
+          {loanApplicationsList.map((eachLoan) => (
+            <li key={eachLoan.id} className="eachLoan">
+              <span>
+                <p>{eachLoan.loan_amount}</p>
+              </span>
+              <span>
+                <p>Status</p>
+                <p>
+                  {' '}
+                  {eachLoan.status}
+                </p>
+              </span>
+              <span>
+                {/* <Link to={`/ReviewLoanApplication/${eachLoan.id}`}>
+                  <button type="button">Review</button>
+                </Link> */}
+
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </>
+  );
+};
 
 export default LoanStatus;
