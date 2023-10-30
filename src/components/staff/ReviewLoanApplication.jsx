1268ae134e4e20795d8fcc7676aab2acb7a70db4
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-30 08:38:57 +0100

    Show loan application details for review

--- src/components/staff/ReviewLoanApplication.jsx
+++ src/components/staff/ReviewLoanApplication.jsx
@@ -5,8 +5,8 @@ const ReviewLoanApplication = () => {
     const [loanDetails, setLoanDetails] = useState({})
     const [serverError, setServerError] = useState(false)
   // Use the useParams hook to get the loan loan_loan_id from the URL
-  const { loan_id } = useParams();
-    const url = `http://localhost:3100/api/v1/staffs/loan_applications/${loan_id}`
+  const { id } = useParams();
+    const url = `http://localhost:3100/api/v1/staffs/loan_applications/${id}`
 
     
   // Fetch the details of the loan with the specified loan_id and display it here
@@ -39,9 +39,32 @@ const ReviewLoanApplication = () => {
       </p>
     ) :
     ( <div>
+      <article>
       <h2>Loan Details</h2>
-      <p>Loan loan_id: {loan_id}</p>
-    {/* Display other loan details here */} 
+      <p>Customer name: {loanDetails.customer_name}</p>
+      <p>Loan amount: {loanDetails.loan_amount}</p>
+      <p>Application Date: {loanDetails.created_at}</p>
+      <p>Purpose: {loanDetails.purpose}</p>
+      <p>Repayment preference: {loanDetails.repayment_preferences}</p>     
+      </article>
+
+
+{/* current reviwed details */}
+      <article>
+      <h2>Current review details</h2>
+      <p>Last review date: {loanDetails.updated_at}</p>
+      <p>Status: {loanDetails.status}</p>
+      <p>Details: {loanDetails.details}</p>
+      <p>Purpose: {loanDetails.purpose}</p>
+      <p>Repayment Schedule: {loanDetails.repayment_schedule}</p>
+      
+      </article>
+    
+
+
+
+
+
     </div>)}
       
     </div>
