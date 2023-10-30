c7ae8c8defc917586e6535852dc49844687b0390
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-30 07:57:01 +0100

    Add fetch API to fetch loan application with unique id

--- src/components/staff/ReviewLoanApplication.jsx
+++ src/components/staff/ReviewLoanApplication.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 
 const ReviewLoanApplication = () => {
-  // Use the useParams hook to get the loan ID from the URL
-  const { id } = useParams();
+    const [loanDetails, setLoanDetails] = useState({})
+  // Use the useParams hook to get the loan loan_loan_id from the URL
+  const { loan_id } = useParams();
+    const url = `http://localhost:3100/api/v1/staffs/loan_applications/${loan_id}`
 
-  // Fetch the details of the loan with the specified ID and display it here
     
+  // Fetch the details of the loan with the specified loan_id and display it here
+
+  useEffect(() => {
+    fetchLoanApplication()
+  }, [])
+
+  const fetchLoanApplication = async () => {
+    try {
+        const response = await fetch(url)
+ 
+        if (response.ok) {
+            const data = await response.json()
+            setLoanDetails(data)
+        }
+        
+    } catch (error) {
+        throw new Error(error);
+      } 
+  }
+
   return (
     <div>
       <h2>Loan Details</h2>
-      <p>Loan ID: {id}</p>
+      <p>Loan loan_id: {loan_id}</p>
       {/* Display other loan details here */}
     </div>
   );
