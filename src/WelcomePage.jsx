5ebbf26a053a177801b61c9565b4fe38096ba3f4
oguntayomicheal <oguntayomicheal@gmail.com>
2023-11-02 03:32:46 +0100

    Add styling to welcome page

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => (
-  <>
+  <div id='welcome_page' className='main_content'>
     <h1>
       Loan App
     </h1>
 
-    <Link to="/staffLogin">
+    <div className="welcome_links">
+          <Link to="/staffLogin">
       Staff Login
     </Link>
 
@@ -22,8 +23,11 @@ const WelcomePage = () => (
     <Link to="/customerRegister">
       Customer Register
     </Link>
+    </div>
 
-  </>
+
+
+  </div>
 );
 
 export default WelcomePage;
