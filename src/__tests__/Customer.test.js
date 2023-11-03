d44d904a46771621d4fec605c95b64d8675704d7
oguntayomicheal <oguntayomicheal@gmail.com>
2023-11-03 03:50:33 +0100

    Update customer test

--- src/__tests__/Customer.test.js
+++ src/__tests__/Customer.test.js
@@ -1,14 +1,15 @@
 import {render} from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import LoanStatus from '../components/customer/LoanStatus'
+import LoanApplicationForm from '../components/customer/LoanApplicationForm'
 
-describe('Customer loan status page', () => {
+describe('Customer page', () => {
     beforeEach(() => {
         localStorage.clear()
        
     })
 
-    test('Renders correctly', () => {
+    test('Renders loan status correctly', () => {
       
         localStorage.setItem('customerData', JSON.stringify({ id: 123 }));
 
@@ -20,5 +21,16 @@ describe('Customer loan status page', () => {
         expect(loanStatus).toMatchSnapshot()
     })
 
+    test('Renders application form correctly', () => {
+      
+      localStorage.setItem('customerData', JSON.stringify({ id: 123 }));
+
+      const loanApplicationForm = render(
+          <Router>
+              <LoanApplicationForm />
+          </Router>
+      )
+      expect(loanApplicationForm).toMatchSnapshot()
+  })
 
 })
